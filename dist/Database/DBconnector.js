da46f5e73857c15a28b706d5403a47e2688eeeb8
Sriram Harshit <sriramharshit4859@gmail.com>
2025-03-31 16:12:10 +0530

    update package.json

--- dist/Database/DBconnector.js
+++ dist/Database/DBconnector.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const path_1 = __importDefault(require("path"));
 const typeorm_1 = require("typeorm");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "sqlite",
-    database: path_1.default.join(__dirname, "../../airports.db"),
-    entities: [path_1.default.join(__dirname, "../entities/*.ts")],
+    database: process.env.DATABASE_PATH || path_1.default.join(__dirname, "../../airports.db"),
+    entities: [path_1.default.join(__dirname, "../entities/*.{js,ts}")],
     synchronize: false,
     logging: false,
 });
--- src/Database/DBconnector.ts
+++ src/Database/DBconnector.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: path.join(__dirname, "../../airports.db"),
-  entities: [path.join(__dirname, "../entities/*.ts")],
+  database:
+    process.env.DATABASE_PATH || path.join(__dirname, "../../airports.db"),
+  entities: [path.join(__dirname, "../entities/*.{js,ts}")],
   synchronize: false,
   logging: false,
 });
